179a66ca6d2312fd0efceaba8dea6ac826c83873
Mohd Arsalan <thesiddiqui7@gmail.com>
2024-10-13 20:35:40 +0530

    update backend link

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000/api/v1', // Replace with your backend URL
+  baseURL: 'https://todo-app-backend1-gv0p.onrender.com/api/v1', // Replace with your backend URL
   headers: {
     'Content-Type': 'application/json',
   },
